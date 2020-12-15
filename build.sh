9ea4bac37df2d8815f0317d61c541ef4660b675d
Benjamin Erhart <berhart@netzarchitekten.com>
2020-12-15 14:46:57 +0100

    Base on latest Snowflake master which contains a lot of patches we previously had to provide ourselves.

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ else
     cd ..
     git clone https://git.torproject.org/pluggable-transports/snowflake.git
     cd snowflake || exit 1
-    git checkout --force --quiet 665d76c5
+    git checkout --force --quiet 3e8947bf
     cd ..
 fi
 
