a08ec936fdd5e70eb94ded159a7471032271ec6a
Benjamin Erhart <berhart@netzarchitekten.com>
2022-07-13 12:35:33 +0200

    Updated to latest Snowflake version 2.3.0.

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ else
     cd ..
     git clone https://git.torproject.org/pluggable-transports/snowflake.git
     cd snowflake || exit 1
-    git checkout --force --quiet 4e7f8975
+    git checkout --force --quiet c983c13a
     cd ..
 fi
 
