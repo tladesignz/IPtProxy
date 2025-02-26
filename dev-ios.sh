0964449fdfb590559f09b841f0fdf8ac872b061e
Benjamin Erhart <berhart@netzarchitekten.com>
2025-02-26 14:29:25 +0100

    Fixed dev build.

--- dev-ios.sh
+++ dev-ios.sh
@@ -2,15 +2,6 @@
 
 cd "$(dirname "$0")" || exit 1
 
-cd lyrebird || exit 1
-git reset --hard
-cd ../snowflake || exit 1
-git reset --hard
-cd ..
-
-patch --directory="lyrebird" --strip=1 < lyrebird.patch
-patch --directory="snowflake" --strip=1 < snowflake.patch
-
 cd "IPtProxy.go" || exit 1
 
 gomobile init
